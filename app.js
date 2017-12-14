6f149067f2b026d776b4fd324d8da7705406d0b2
reach2deep <reach2deep@gmail.com>
2017-12-14 12:34:27 +0800

    Port update to set dynamically

--- app.js
+++ app.js
@@ -7,32 +7,32 @@ var bodyParser = require('body-parser');
 var routes = require('./api/routes');
 
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', (process.env.PORT || 3000));
 
 // Add headers
-app.use(function (req, res, next) {
-  
-      // Website you wish to allow to connect
-      res.setHeader('Access-Control-Allow-Origin', '*');
-  
-      // Request methods you wish to allow
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  
-      // Request headers you wish to allow
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  
-      // Set to true if you need the website to include cookies in the requests sent
-      // to the API (e.g. in case you use sessions)
-      res.setHeader('Access-Control-Allow-Credentials', true);
-  
-      // Pass to next layer of middleware
-      next();
-  });
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // Add middleware to console log every request
 app.use(function(req, res, next) {
-  console.log(req.method, req.url);
-  next(); 
+    console.log(req.method, req.url);
+    next();
 });
 
 // Set static directory before defining routes
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Enable parsing of posted forms
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // Add some routing
 app.use('/api', routes);
@@ -49,6 +49,6 @@ app.use('/api', routes);
 
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Magic happens on port ' + port);
+    var port = server.address().port;
+    console.log('Magic happens on port ' + port);
 });
