709c8eb2a1d0da1320a39d6daab37f93af3c84aa
deep <reach2deep@gmail.com>
2018-07-23 23:53:10 +0800

    async added in mater controller

--- api/controllers/master.controller.js
+++ api/controllers/master.controller.js
@@ -1,47 +1,99 @@
 var mongoose = require('mongoose');
 var moment = require('moment');
+var async = require("async");
 var Category = mongoose.model('Category');
 var Payee = mongoose.model('Payee');
 
 
 // GET ALL CATEGORY
 module.exports.getAllMasterData = function (req, res) {
-   var masterData={};
-   var categoryData
-   var payeeData 
-    Category
+
+       var masterData={};
+   var categoryData;
+   var payeeData ;
+    async.parallel({
+        one: function(callback) {
+            console.log('one');
+                Category
         .find()
         .exec(function (err, categories) {
             if (err) {
-                console.log("Error retreiving data");
-                res
-                    .status(500)
-                    .json(err);
+                console.log("Error retreiving categories data");              
+                    callback(err, null);
             }
             console.log('Found categories', categories.length);
-            masterData.categories=categories;
+            callback(null, categories);           
         });
 
-        Payee
-        .find()
-        .exec(function (err, payees) {
-            if (err) {
-                console.log("Error retreiving data");
-                res
+            // setTimeout(function() {
+            //     callback(null, 1);
+            // }, 200);
+        },
+        two: function(callback) {
+            console.log('two');
+            Payee
+            .find()
+            .exec(function (err, payees) {
+                if (err) {
+                    console.log("Error retreiving payees data");              
+                        callback(err, null);
+                }
+                console.log('Found payees', payees.length);
+                callback(null, payees);           
+            });
+        }
+    }, function(err, results) {
+        if(err)
+        {
+                            res
                     .status(500)
                     .json(err);
-            }
-            console.log('Found payees', payees.length);
-            //payeeData = payees;
-            masterData.payees= payees;
-            res.status(200)
-            .json(masterData);
-        });
+        }
+      //  console.log(results);
+        masterData.categories=results.one;
+        masterData.payees= results.two;
+       // console.log(JSON.stringify(masterData));
+        res.status(200)
+                     .json(masterData);
+        // results is now equals to: {one: 1, two: 2}
+    });
+
+//    var masterData={};
+//    var categoryData
+//    var payeeData 
+//     Category
+//         .find()
+//         .exec(function (err, categories) {
+//             if (err) {
+//                 console.log("Error retreiving data");
+//                 res
+//                     .status(500)
+//                     .json(err);
+//             }
+//             console.log('Found categories', categories.length);
+//             masterData.categories=categories;
+//         });
+
+//         Payee
+//         .find()
+//         .exec(function (err, payees) {
+//             if (err) {
+//                 console.log("Error retreiving data");
+//                 res
+//                     .status(500)
+//                     .json(err);
+//             }
+//             console.log('Found payees', payees.length);
+//             //payeeData = payees;
+//             masterData.payees= payees;
+//             res.status(200)
+//             .json(masterData);
+//         });
 
        //  masterData.category= categoryData;
        //  masterData.payees= payeeData;
 
     //    console.log(JSON.stringify(categoryData));
     //    console.log(JSON.stringify(payeeData));
-    //    return res.json('TEST');
+      // return res.json('TEST');
 };
